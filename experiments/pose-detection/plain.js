f23f5719c2c06abf5da54db77174b54211ce240a
Kenshin7 <rithyka9@gmail.com>
2024-04-13 00:12:52 -0400

    not working

---
+++ experiments/pose-detection/plain.js
@@ -0,0 +1,68 @@
+let pose = [],
+	skeleton = [];
+let size = 8;
+
+
+//https://stackoverflow.com/a/4429862
+const video = document.getElementById("video");
+const c = document.getElementById("canvas");
+const ctx = c.getContext("2d");
+
+video.addEventListener("loadedmetadata", () => {
+	c.width = video.videoWidth;
+	c.height = video.videoHeight;
+});
+
+function draw() {
+	requestAnimationFrame(draw);
+
+	//video
+	ctx.drawImage(video, 0, 0, c.width, c.height);
+
+	//skeleton and pose
+	ctx.fillStyle = "#00f";
+	for(let [x, y] of skeleton) {
+		ctx.fillRect(x - 0.5 * size, y - 0.5 * size, size, size);
+	}
+
+	ctx.fillStyle = "#ff8000";
+	for(let [x, y] of pose) {
+		ctx.fillRect(x - 0.5 * size, y - 0.5 * size, size, size);
+	}
+}
+video.addEventListener("play", draw);
+
+
+
+//https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia#examples
+navigator.mediaDevices
+	.getUserMedia({ video: { facingMode: "environment" } })
+	.then((stream) => {
+		video.srcObject = stream;
+		video.onloadedmetadata = () => { video.play(); };
+	})
+	.catch((ex) => {
+		console.error(ex);
+	});
+
+
+
+//https://learn.ml5js.org/#/reference/posenet
+const posenet = ml5.poseNet(
+	video,
+	{
+		detectionType: "single"
+	},
+	() => {
+		console.log("posenet loaded");
+	});
+
+let getXY = (x) => ([x["position"]["x"], x["position"]["y"]]);
+posenet.on("pose", (results) => {
+	if(results.length == 0 || results[0]["skeleton"].length == 0) return;
+
+	pose = results[0]["pose"]["keypoints"].map(getXY);
+	skeleton = results[0]["skeleton"][0].map(getXY);
+});
+
+
